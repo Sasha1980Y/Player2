5566716e4eab7cbdd1185b5054061cf08ca561a4
alexanderyakovenko <yakovenko5893@gmail.com>
2018-03-09 17:41:20 +0200

    worked version

--- Player2/ViewController.swift
+++ Player2/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    var player:AVPlayer?
+    var playerItem:AVPlayerItem?
+    var playButton:UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //let url = URL(string: "https://s3.amazonaws.com/kargopolov/kukushka.mp3")
+        //let url = URL(string: "http://192.168.0.120:8000/test_audio_stream")
+        //http://radio02-cn03.akadostream.ru:8113/radioromantika128.mp3
+        let url = URL(string: "http://radio02-cn03.akadostream.ru:8113/radioromantika128.mp3")
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
+        
+        let playerLayer = AVPlayerLayer(player: player!)
+        playerLayer.frame=CGRect(x:0, y:0, width:10, height:50)
+        self.view.layer.addSublayer(playerLayer)
+        
+        playButton = UIButton(type: UIButtonType.system) as UIButton
+        let xPostion:CGFloat = 50
+        let yPostion:CGFloat = 100
+        let buttonWidth:CGFloat = 150
+        let buttonHeight:CGFloat = 45
+        
+        playButton!.frame = CGRect(x: xPostion, y: yPostion, width: buttonWidth, height: buttonHeight)
+        playButton!.backgroundColor = UIColor.lightGray
+        playButton!.setTitle("Play", for: UIControlState.normal)
+        playButton!.tintColor = UIColor.black
+        playButton!.addTarget(self, action: #selector(ViewController.playButtonTapped(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(playButton!)
     }
-
+    
+    @objc func playButtonTapped(_ sender:UIButton)
+    {
+        if player?.rate == 0
+        {
+            player!.play()
+            //playButton!.setImage(UIImage(named: "player_control_pause_50px.png"), forState: UIControlState.Normal)
+            playButton!.setTitle("Pause", for: UIControlState.normal)
+        } else {
+            player!.pause()
+            //playButton!.setImage(UIImage(named: "player_control_play_50px.png"), forState: UIControlState.Normal)
+            playButton!.setTitle("Play", for: UIControlState.normal)
+        }
+    }
 
 }
 
